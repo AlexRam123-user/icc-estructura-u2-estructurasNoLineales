677c268b008e7ce54d43fbfd546af1b727ffe2d8
AlexRam456-user <alex2004rx@gmail.com>
2025-01-13 12:59:23 -0500

    Arbol AVL

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 import java.util.List;
 
 import main.Ejercicio_03_listLevels.ListLevels;
+import main.Materia.Controllers.ArbolAVL;
 import main.Materia.Controllers.ArbolBinario;
 import main.Materia.Controllers.ArbolRecorridos;
 import main.Materia.Models.Node;
@@ -10,7 +11,8 @@ public class App {
         System.out.println("Hello, World!");
 
         // runArbolBinario();
-        runEjercicio3();
+        //runEjercicio3();
+        runArbolAvl();
     }
 
     public static void runEjercicio3() {
@@ -69,6 +71,17 @@ public class App {
 
         arbolRecorridos.inOrderRecursivo(arbolBinario.getRoot());
 
+    }
+
+    private static void runArbolAvl(){
+        ArbolAVL arbolAVL = new ArbolAVL();
+        int [] values = {10, 20, 15, 24, 9, 8, 21, 23, 50, 25};
+
+        for(int valor : values){
+            arbolAVL.insert(valor);
+        }
+
+        arbolAVL.printTree();
     }
 }
 
